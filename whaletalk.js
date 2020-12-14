29da60bdcf1a890eadce00ddd5b07b2cf9d71fb6
zealsham <shammahagwor@gmail.com>
2020-12-14 23:55:37 +0100

    whaletalk done , i hate secret message as it involves calling js methods .

---
+++ whaletalk.js
@@ -0,0 +1,59 @@
+/*
+Take a phrase like ‘turpentine and turtles’ and translate it into its “whale talk” equivalent: ‘UUEEIEEAUUEE’.
+
+There are a few simple rules for translating text to whale language:
+
+There are no consonants. Only vowels excluding “y”.
+The u‘s and e‘s are extra long, so we must double them in our program.
+
+*/
+const prompt = require('prompt-sync')()
+const getUserInput =()=>{
+      let input = prompt("Enter the text to be converted to whale talk")
+      return input
+}
+
+const sanitizeInput =()=>{
+    let inputToSantize = getUserInput()
+    //ensure consecutive digits is not entered 
+    let containNumber = parseInt(inputToSantize)
+    if(!isNaN(containNumber)){
+        console.log("do not enter consecutive digits as input to this program")
+        return
+    }
+    return inputToSantize.toUpperCase()
+
+}
+const isVowel = ()=>{
+    let alphabet = sanitizeInput()
+    if(typeof alphabet ==='undefined'){
+        console.log("you probably entered consecutive digits")
+        return
+    }
+    let vowelsInInput = []
+    const vowels=['A','E','I','O','U','Y'] //shh i no 'Y' isn't a vowel but you know 
+    for(let i=0; i<alphabet.length; i++){
+        if(vowels.indexOf(alphabet[i]) > -1){ // could have used Array.includes but just decided to stick wit this 
+             vowelsInInput.push(alphabet[i])
+        } 
+
+    }
+    return vowelsInInput
+}
+
+const whaleTalkLogic = (arrayOfVowels)=>{
+    let whaleTalkArray = []
+    for(let i =0; i<arrayOfVowels.length; i++){
+        if(arrayOfVowels[i]==="U" || arrayOfVowels[i]==="E"){
+            let WhaleChar= arrayOfVowels[i]+arrayOfVowels[i]
+            whaleTalkArray.push(WhaleChar)
+            continue
+        }
+        whaleTalkArray.push(arrayOfVowels[i])
+    }
+    console.log(whaleTalkArray)
+    return whaleTalkArray.join('')
+}
+
+let textToBeTranslated = isVowel()
+console.log(whaleTalkLogic(textToBeTranslated))
